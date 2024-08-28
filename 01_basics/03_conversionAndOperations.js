94f2d658d109a6ffb19f53076e66a252203e82b8
Mr-Deepak-Pandey <56496567+Mr-Deepak-Pandey@users.noreply.github.com>
2024-08-28 18:32:46 +0000

    datatype conversion

---
+++ 01_basics/03_conversionAndOperations.js
@@ -0,0 +1,32 @@
+let score="33abc";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let value=Number(score);
+console.log(typeof value)
+console.log(value)
+
+//datatype conversion to int
+//"33"=>33
+//"33abc "=>Nan
+//true=>1, false=>0
+//null=>0
+//undefined=>Nan
+
+let val="deep"
+
+let bool=Boolean(val)
+console.log(typeof bool)
+console.log(bool)
+
+//datatype conversion to boolean
+//1=>true 0=>false
+// ""=>false
+//"deep"=>true
+
+let num=33
+let str=String(num)
+console.log(typeof str)
+console.log(str)
+
