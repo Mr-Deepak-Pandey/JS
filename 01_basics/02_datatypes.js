a65334e343d3d28165bad978d3832cff0bcdf810
Mr-Deepak-Pandey <56496567+Mr-Deepak-Pandey@users.noreply.github.com>
2024-08-28 15:36:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using node js ,not browser.it have different syntax here in node js
+ 
+console.log(3+3)//code readibility should be high
+console.log("Deepak")
+
+let name="Deepak"
+let age=18
+let isLoggedIn=false
+
+//number=>2 to power either 53 or 52
+//big int
+//string=> ""
+//boolean=true/false
+//null=>standalone value
+//undefined mtlb abhi value assign nhi hui hi
+//symbol=>unique
+
+
+//object
+
+console.log(typeof "Deepak");
+console.log(typeof age);
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object 
