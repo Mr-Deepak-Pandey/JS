cfed104e49f993db543d16dcdd9dcc95a18afe21
Mr-Deepak-Pandey <56496567+Mr-Deepak-Pandey@users.noreply.github.com>
2025-01-23 14:20:09 +0000

    while aur do while loop

---
+++ 05_iterators/two.js
@@ -0,0 +1,32 @@
+//while loop
+
+// while(condition)
+// {
+//     //opertion
+//     //increment ,decrement
+//     //ya kkoi condition which terminates the loop
+// }
+
+let i=0
+while(i<10)
+{
+    console.log(i)
+    i++
+}
+
+let array=["shaktiman","Junior G","aryaman"]
+
+let arrIndex=0
+while(arrIndex<array.length)
+{
+    console.log(array[arrIndex])
+    arrIndex++
+}
+
+
+// do while loop
+let score =11
+do{
+console.log(`score is ${score}`)
+score=score+1
+}while(score<=10);
